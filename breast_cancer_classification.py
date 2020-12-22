2259064c1c1a91def647d41a031a638f54c9606a
Jeremy <engjat94@gmail.com>
2020-12-22 07:26:12 -0600

    adding the basic selection models for classification

--- breast_cancer_classification.py
+++ breast_cancer_classification.py
@@ -5,3 +5,4 @@ import seaborn as sns # Statistical data visualization
 # Import Cancer data drom the Sklearn library
 from sklearn.datasets import load_breast_cancer
 cancer = load_breast_cancer()
+
