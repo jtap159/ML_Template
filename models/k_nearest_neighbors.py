d92b4f028e35bc83f810197cc1bf0fff77f4497e
Jeremy <engjat94@gmail.com>
2020-12-22 09:05:05 -0600

    added k-fold cross validation to the models and added the grid_search.py

--- models/k_nearest_neighbors.py
+++ models/k_nearest_neighbors.py
@@ -31,3 +31,9 @@ y_pred = classifier.predict(X_test)
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
 accuracy_score(y_test, y_pred)
+
+# Applying k-Fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
